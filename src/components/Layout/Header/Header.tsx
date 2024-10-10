7e662d77bd9b708f80e5949aa28a2fb50d18f06a
Thanh Phong <pttp15122002@gmail.com>
2024-10-10 16:51:36 +0700

    Feat: header section

---
+++ src/components/Layout/Header/Header.tsx
@@ -0,0 +1,9 @@
+import ThemeToggle from "@/components/Buttons/ThemeToggle";
+
+export default function Header() {
+  return (
+    <header className="max-w-7xl mx-auto py-5 px-5 lg:px-0">
+      <ThemeToggle />
+    </header>
+  );
+}
